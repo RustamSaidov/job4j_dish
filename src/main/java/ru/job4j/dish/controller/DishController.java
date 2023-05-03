1e4dfa834db6ef457db4a9f7542fd3307876de22
Qafas <rustam_ussr@mail.ru>
2023-05-03 20:26:53 +0400

    2. Монолитные системы [#338433]

--- src/main/java/ru/job4j/dish/controller/DishController.java
+++ src/main/java/ru/job4j/dish/controller/DishController.java
@@ -1,4 +1,46 @@
 package ru.job4j.dish.controller;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.dish.model.Dish;
+import ru.job4j.dish.service.DishService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/dish")
+@Data
+@Slf4j
 public class DishController {
+
+    private final DishService dishService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Dish>> getAll() {
+        return ResponseEntity.of(Optional.of(dishService.findAll()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Dish> findById(@PathVariable int id) {
+        var site = this.dishService.findById(id);
+        return new ResponseEntity<Dish>(
+                site.orElse(new Dish()),
+                site.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        try {
+            this.dishService.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception exception) {
+            log.error("An error occurred while deleting Dish with id {}", id, exception);
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/ru/job4j/dish/service/DishService.java
+++ src/main/java/ru/job4j/dish/service/DishService.java
@@ -1,21 +1,34 @@
 package ru.job4j.dish.service;
 
+import lombok.AllArgsConstructor;
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Service;
 import ru.job4j.dish.model.Dish;
+import ru.job4j.dish.repository.DishRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface DishService {
+@ThreadSafe
+@Service
+@AllArgsConstructor
+public class DishService {
+    private final DishRepository dishRepository;
 
-    Dish save(Dish dish);
+    public Dish save(Dish dish) {
+        return dishRepository.save(dish);
+    }
 
-    boolean deleteById(int id);
+    public boolean deleteById(int id) {
+        return dishRepository.deleteById(id);
+    }
 
-    boolean update(Dish dish);
 
-    Optional<Dish> findById(int id);
+    public Optional<Dish> findById(int id) {
+        return dishRepository.findById(id);
+    }
 
-    List<Dish> findAll();
-
-    Optional<Dish> findByName(String name);
+    public List<Dish> findAll() {
+        return dishRepository.findAll();
+    }
 }
